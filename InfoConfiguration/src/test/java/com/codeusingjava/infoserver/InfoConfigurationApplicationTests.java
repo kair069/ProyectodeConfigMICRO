eb9298e99d8f029bf0f87aa780061ac64a40b192
kair069 <81661845+kair069@users.noreply.github.com>
2023-07-10 23:52:33 -0500

    Add files via upload

---
+++ InfoConfiguration/src/test/java/com/codeusingjava/infoserver/InfoConfigurationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codeusingjava.infoserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InfoConfigurationApplicationTests{
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
